848f6c676b386b7ecf4ac9d49e77352e59ee5552
Simon Murray <simondonaldmurray@gmail.com>
2022-04-27 15:04:20 +0100

    Update gse_url.sh

--- scripts/gse_url.sh
+++ scripts/gse_url.sh
@@ -2,4 +2,4 @@
 
 file=$1
 
-tail -n +2 $file | cut -f 7 |  tr ";" "\n" | while read i; do j=`basename $i`; k=${j%%.fastq.gz}; wget $i -o $k.wget.log; done
+tail -n +2 $file | cut -f 7 |  tr ';' '\n' | while read i; do j=`basename $i`; k=${j%%.fastq.gz}; wget $i -o $k.wget.log; done
