f39cebb0776cb5e9a808f3407f504b3d4b15a407
Alexander Predeus <ap41@sanger.ac.uk>
2023-03-24 11:02:11 +0000

    Fix STARsolo wrapper UMILEN bug, BAM option changes

--- scripts/starsolo_10x_auto.sh
+++ scripts/starsolo_10x_auto.sh
@@ -1,13 +1,22 @@
 #!/bin/bash -e 
 
-## v3.0 of STARsolo wrappers is set up to guess the chemistry automatically
-## newest version of the script uses STAR v2.7.9a with EM multimapper processing in STARsolo (but it's not on by default; turn this on with --soloMultiMappers EM)
-## velocyto extra processing also became unnecessary 
+## v3.1 of STARsolo wrappers is set up to guess the chemistry automatically
+## newest version of the script uses STAR v2.7.10a with EM multimapper processing 
+## in STARsolo which on by default; the extra matrix can be found in /raw subdir 
 
-SIF=/nfs/cellgeni/singularity/images/starsolo_2-7-10a-alpha-220818_samtools_1-15-1_seqtk-1-13_bbmap_38-97_RSEM-1-3-3.sif
+SIF="/nfs/cellgeni/singularity/images/starsolo_2-7-10a-alpha-220818_samtools_1-15-1_seqtk-1-13_bbmap_38-97_RSEM-1-3-3.sif"
+CMD="singularity run --nv --bind /nfs,/lustre $SIF"
 
 FQDIR=$1
 SERIES=$2
+
+if [[ $FQDIR == "" || $SERIES == "" ]]
+then
+  >&2 echo "Usage: ./starsolo_10x_auto.sh <fastq_dir> <series_id (GSE,E-MTAB,PRJNA)>"
+  >&2 echo "(make sure you set the correct REF, WL, and BAM variables below)"
+  exit 1
+fi
+
 ## this is BSUB job array environmental variable
 TAG=`head -$LSB_JOBINDEX $SERIES.sample.list | tail -1`
 
@@ -26,19 +35,13 @@ else
   exit 1
 fi
 
-if [[ $FQDIR == "" || $SERIES == "" ]]
-then
-  >&2 echo "Usage: ./starsolo_10x_auto.sh <full_path_to_fastq_dir> <series_id (GSE,E-MTAB,PRJNA)>"
-  >&2 echo "(make sure you set the correct REF, WL, and SORTEDBAM/NOBAM variables below)"
-  exit 1
-fi
-
+FQDIR=`readlink -f $FQDIR`
 CPUS=16                                                                ## typically bsub this into normal queue with 16 cores and 64 Gb RAM.   
 REF=/nfs/cellgeni/STAR/$SHORTSP/2020A/index                               ## choose the appropriate reference 
 WL=/nfs/cellgeni/STAR/whitelists                                       ## directory with all barcode whitelists
 
 ## choose one of the two otions, depending on whether you need a BAM file 
-#BAM="--outSAMtype BAM SortedByCoordinate --outBAMsortingThreadN 2 --limitBAMsortRAM 120000000000 --outMultimapperOrder Random --runRNGseed 1 --outSAMattributes NH HI AS nM CB UB GX GN"
+#BAM="--outSAMtype BAM SortedByCoordinate --outBAMsortingBinsN 500 --limitBAMsortRAM 60000000000 --outMultimapperOrder Random --runRNGseed 1 --outSAMattributes NH HI AS nM CB UB CR CY UR UY GX GN"
 BAM="--outSAMtype None"
 
 ###################################################################### DONT CHANGE OPTIONS BELOW THIS LINE ##############################################################################################
@@ -66,11 +69,11 @@ else
   exit 1
 fi 
 
-## also define one file from R1/R2; we choose the largest one  
+## also define one file from R1/R2; we choose the largest one, because sometimes there are tiny files from trial runs
 R1F=`echo $R1 | tr ',' ' ' | xargs ls -s | tail -n1 | awk '{print $2}'`
 R2F=`echo $R2 | tr ',' ' ' | xargs ls -s | tail -n1 | awk '{print $2}'`
 
-## let's see if the files are archived or not. Gzip is the only one we test for, but bgzip archives should work too since they are gzip-compatible.
+## let's see if the files are archived or not. Gzip is the most common, but bgzip archives should work too since they are gzip-compatible.
 GZIP=""
 BC=""
 NBC1=""
@@ -83,8 +86,8 @@ R1DIS=""
 
 
 ## randomly subsample 200k reads - let's hope there are at least this many (there should be):
-singularity run --nv --bind /nfs,/lustre $SIF seqtk sample -s100 $R1F 200000 > test.R1.fastq &
-singularity run --nv --bind /nfs,/lustre $SIF seqtk sample -s100 $R2F 200000 > test.R2.fastq &
+$CMD seqtk sample -s100 $R1F 200000 > test.R1.fastq &
+$CMD seqtk sample -s100 $R2F 200000 > test.R2.fastq &
 wait
 
 ## see if the original fastq files are archived: 
@@ -102,12 +105,13 @@ R2LEN=`cat test.R2.fastq | awk 'NR%4==2' | awk '{sum+=length($0)} END {printf "%
 R1DIS=`cat test.R1.fastq | awk 'NR%4==2' | awk '{print length($0)}' | sort | uniq -c | wc -l`
 
 ## elucidate the right barcode whitelist to use. Grepping out N saves us some trouble. Note the special list for multiome experiments (737K-arc-v1.txt):
-if (( $NBC2 > 80000 )) 
+## 80k (out of 200,000) is an empirical number - I've seen <50% barcodes matched to the whitelist, but a number that's < 40% suggests something is very wrong
+if (( $NBC3 > 80000 )) 
 then 
-  BC=$WL/737K-august-2016.txt
-elif (( $NBC3 > 80000 ))
-then
   BC=$WL/3M-february-2018.txt
+elif (( $NBC2 > 80000 ))
+then
+  BC=$WL/737K-august-2016.txt
 elif (( $NBCA > 80000 ))
 then
   BC=$WL/737K-arc-v1.txt
@@ -137,25 +141,32 @@ then
   exit 1
 fi
 
-## assign the necessary variables for barcode/UMI length/paired-end processing:  
+## assign the necessary variables for barcode/UMI length/paired-end processing. 
+## scripts was changed to not rely on read length for the UMIs because of the epic Hassan case
+# (v2 16bp barcodes + 10bp UMIs were sequenced to 28bp, effectively removing the effects of the UMIs)
 if (( $R1LEN > 50 )) 
 then
   PAIRED=True
-  UMILEN=10
+fi
+
+if [[ $BC == "$WL/3M-february-2018.txt" || $BC == "$WL/737K-arc-v1.txt" ]] 
+then 
   CBLEN=16
-elif (( $NBC1 > 80000 )) 
+  UMILEN=12
+elif [[ $BC == "$WL/737K-august-2016.txt" ]] 
+then
+  CBLEN=16
+  UMILEN=10
+elif [[ $BC == "$WL/737K-april-2014_rc.txt" ]] 
 then
   CBLEN=14
-  UMILEN=$((R1LEN-14))
-else
-  CBLEN=16
-  UMILEN=$((R1LEN-16)) 
+  UMILEN=10
 fi 
 
-## finally, see if you have 5' or 3' experiment. I don't know and easier way:  
+## finally, see if you have 5' or 3' experiment. I don't know and easier way than to run a test alignment:  
 STRAND=Forward
 
-singularity run --nv --bind /nfs,/lustre $SIF STAR --runThreadN $CPUS --genomeDir $REF --readFilesIn test.R2.fastq test.R1.fastq --runDirPerm All_RWX --outSAMtype None \
+$CMD STAR --runThreadN $CPUS --genomeDir $REF --readFilesIn test.R2.fastq test.R1.fastq --runDirPerm All_RWX --outSAMtype None \
      --soloType CB_UMI_Simple --soloCBwhitelist $BC --soloBarcodeReadLength 0 --soloCBlen $CBLEN --soloUMIstart $((CBLEN+1)) \
      --soloUMIlen $UMILEN --soloStrand Forward \
      --soloUMIdedup 1MM_CR --soloCBmatchWLtype 1MM_multi_Nbase_pseudocounts --soloUMIfiltering MultiGeneUMI_CR \
@@ -178,10 +189,6 @@ fi
 if [[ $STRAND == "Forward" && $PAIRED == "True" ]]
 then
   PAIRED=False
-  if [[ $BC == "$WL/3M-february-2018.txt" ]] 
-  then
-    UMILEN=12
-  fi
 fi
 
 echo "Done setting up the STARsolo run; here are final processing options:"
@@ -201,13 +208,14 @@ echo "--------------------------------------------------------------------------
 
 if [[ $PAIRED == "True" ]]
 then
-  singularity run --nv --bind /nfs,/lustre $SIF STAR --runThreadN $CPUS --genomeDir $REF --readFilesIn $R1 $R2 --runDirPerm All_RWX $GZIP $BAM --soloBarcodeMate 1 --clip5pNbases 39 0 \
+  ## note the R1/R2 order of input fastq reads and --soloStrand Forward for 5' paired-end experiment
+  $CMD STAR --runThreadN $CPUS --genomeDir $REF --readFilesIn $R1 $R2 --runDirPerm All_RWX $GZIP $BAM --soloBarcodeMate 1 --clip5pNbases 39 0 \
      --soloType CB_UMI_Simple --soloCBwhitelist $BC --soloCBstart 1 --soloCBlen $CBLEN --soloUMIstart $((CBLEN+1)) --soloUMIlen $UMILEN --soloStrand Forward \
      --soloUMIdedup 1MM_CR --soloCBmatchWLtype 1MM_multi_Nbase_pseudocounts --soloUMIfiltering MultiGeneUMI_CR \
      --soloCellFilter EmptyDrops_CR --outFilterScoreMin 30 \
      --soloFeatures Gene GeneFull Velocyto --soloOutFileNames output/ features.tsv barcodes.tsv matrix.mtx --soloMultiMappers EM --outReadsUnmapped Fastx
 else 
-  singularity run --nv --bind /nfs,/lustre $SIF STAR --runThreadN $CPUS --genomeDir $REF --readFilesIn $R2 $R1 --runDirPerm All_RWX $GZIP $BAM \
+  $CMD STAR --runThreadN $CPUS --genomeDir $REF --readFilesIn $R2 $R1 --runDirPerm All_RWX $GZIP $BAM \
      --soloType CB_UMI_Simple --soloCBwhitelist $BC --soloBarcodeReadLength 0 --soloCBlen $CBLEN --soloUMIstart $((CBLEN+1)) --soloUMIlen $UMILEN --soloStrand $STRAND \
      --soloUMIdedup 1MM_CR --soloCBmatchWLtype 1MM_multi_Nbase_pseudocounts --soloUMIfiltering MultiGeneUMI_CR \
      --soloCellFilter EmptyDrops_CR --clipAdapterType CellRanger4 --outFilterScoreMin 30 \
@@ -217,7 +225,7 @@ fi
 ## index the BAM file
 if [[ -s Aligned.sortedByCoord.out.bam ]]
 then
-  singularity run --nv --bind /nfs,/lustre $SIF samtools index -@16 Aligned.sortedByCoord.out.bam
+  $CMD samtools index -@16 Aligned.sortedByCoord.out.bam
 fi
 
 ## finally, let's gzip all outputs
