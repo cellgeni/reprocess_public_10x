80d543c715db7d651bb63d4636076a51dc19fc59
Aljes <aleksandr.binkevich@gmail.com>
2024-12-13 12:36:30 +0000

    Replaced runinfo file with xml file with full meta

--- scripts/curl_ena_metadata.sh
+++ scripts/curl_ena_metadata.sh
@@ -15,24 +15,26 @@ KK=`cat $RUNS`
 
 for i in $KK
 do
-  >&2 echo "Processing run ID $i.."
+  >&2 echo "Loading ENA metadata for $i.."
   curl "https://www.ebi.ac.uk/ena/portal/api/filereport?accession=$i&result=read_run&fields=study_accession,secondary_study_accession,sample_accession,secondary_sample_accession,experiment_accession,experiment_alias,run_accession,run_alias,tax_id,scientific_name,fastq_ftp,submitted_ftp,sra_ftp&format=tsv&download=true&limit=0" 2> /dev/null | sed '/study_accession/d' >> $SERIES.ena.tsv
 done
 
 
-if [[ ! -s $SERIES.ena.tsv &&  $SERIES == GSE* ]]
+if [[ $SERIES == GSE* ]]
 then
-  >&2 echo "WARNING: Was not able to load metadata from ENA. Loading it from SRA..."
-  rm $SERIES.ena.tsv
-  : > $SERIES.sra.tsv
+  >&2 echo "Loading SRA metadata for $i..."
   for i in $KK
   do
     wget --quiet --output-document="$i.xml" "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=sra&term=${i}&usehistory=y"
     WebEnv=$(grep -oP '<WebEnv>\K[^<]+' $i.xml)
     QueryKey=$(grep -oP '<QueryKey>\K[^<]+' $i.xml)
     rm -f $i.xml
-    curl "https://trace.ncbi.nlm.nih.gov/Traces/sra-db-be/sra-db-be.cgi?rettype=runinfo&WebEnv=${WebEnv}&query_key=${QueryKey}" 2> /dev/null | sed '/BioProject/d' | sed  's/,/\t/g' > $SERIES.sra.tsv
+    curl "https://trace.ncbi.nlm.nih.gov/Traces/sra-db-be/sra-db-be.cgi?rettype=exp&WebEnv=${WebEnv}&query_key=${QueryKey}" 2> /dev/null > $i.sra.xml
+    ./parse_sra_xml.py --output $i.sra.tsv  $i.sra.xml
+    rm $i.sra.xml
   done
 fi
+
+cat *.sra.tsv > $SERIES.sra.tsv
 
 >&2 echo "CURL METADATA: ALL DONE!"
