177d57a510d02ba8d09340edf0d287adf680622a
cellgeni <cellgeni@sanger.ac.uk>
2025-01-17 12:59:31 +0000

    Changed the script to have the same structure as `curl_ena_metadata.sh`

--- scripts/curl_sra_metadata.sh
+++ scripts/curl_sra_metadata.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 
-SERIES=$1
-RUNS="$SERIES.project.list"
+RUNS=$1
 
 if [[ ! -f $RUNS ]]
 then
@@ -18,9 +17,7 @@ do
   WebEnv=$(grep -oP '<WebEnv>\K[^<]+' $i.xml)
   QueryKey=$(grep -oP '<QueryKey>\K[^<]+' $i.xml)
   rm -f $i.xml
-  curl "https://trace.ncbi.nlm.nih.gov/Traces/sra-db-be/sra-db-be.cgi?rettype=runinfo&WebEnv=${WebEnv}&query_key=${QueryKey}" 2> /dev/null | sed '/BioProject/d' | sed  's/,/\t/g' > $i.sra.tsv
+  curl "https://trace.ncbi.nlm.nih.gov/Traces/sra-db-be/sra-db-be.cgi?rettype=runinfo&WebEnv=${WebEnv}&query_key=${QueryKey}" 2> /dev/null | sed '/BioProject/d' | sed  's/,/\t/g'
 done
-
-cat *.sra.tsv > $SERIES.sra.tsv
 
 >&2 echo "CURL SRA METADATA: ALL DONE!"
