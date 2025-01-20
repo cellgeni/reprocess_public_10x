fe2a7831694701b5b326f02721c8e695a2904e91
cellgeni <cellgeni@sanger.ac.uk>
2025-01-20 16:32:21 +0000

    Added full paths

--- tests/test_metadata.sh
+++ tests/test_metadata.sh
@@ -8,7 +8,7 @@ fi
 
 SERIES=$1
 SAMPLE_LIST=${2:-""}
-OUTPUT_DIR="output/$SERIES"
+OUTPUT_DIR="${GITHUB_WORKSPACE}/output/$SERIES"
 
 # Create output directory and copy all scripts
 mkdir -p $OUTPUT_DIR
@@ -20,7 +20,7 @@ echo "Loading metadata for $SERIES $SAMPLE_LIST"
 ./collect_metadata.sh $SERIES $SAMPLE_LIST
 
 # Test output
-for file in test_data/$SERIES/*
+for file in ${GITHUB_WORKSPACE}/test_data/$SERIES/*
 do
   filename=$(basename $file)
   echo "Testing $file"
@@ -40,7 +40,7 @@ do
   fi
 
   # Compare the actual output with the expected output
-  if ! diff -q $filename $file
+  if [[ ! diff -q $filename $file ]]
   then
     echo "❌ERROR: Output file $filename does not match expected output!"
     exit 1
